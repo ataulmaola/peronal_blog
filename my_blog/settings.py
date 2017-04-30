471cfb93111820bfb3a0ce57947258f0f1baf7bd
ataulmaola <ataulanas65@gmail.com>
2017-04-30 07:27:08 +0600

    added crispy form

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -36,9 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'blog_app',
 ]
-
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
