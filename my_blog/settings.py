cf0342a868b360b0ee9c6ac72ee6dbda8017965b
ataulmaola <ataulanas65@gmail.com>
2017-04-30 06:48:25 +0600

    new setting

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY=config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [r'ataulanas.herokuapp.com/blog_app']
+ALLOWED_HOSTS = ['ataulanas.herokuapp.com']
 
 
 # Application definition
